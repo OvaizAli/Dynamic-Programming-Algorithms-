0da030c2b3584747888175da3c73e287dbf61251
Zaeem <ahmedzaeem32@gmail.com>
2020-12-24 10:37:12 +0500

    final version(3)

--- Web/main.py
+++ Web/main.py
@@ -1,7 +1,7 @@
 import eel
 # E:/5th Semester/Fall 2020/ALGO/Sir Zeshan/Dynamic-Programming-Algorithms-/Web
 # eel.init('E:/5th Semester/Fall 2020/ALGO/Sir Zeshan/Dynamic-Programming-Algorithms-/Web')
-eel.init('E:/5th Semester/Fall 2020/ALGO/Sir Zeshan/Dynamic-Programming-Algorithms-/Web')
+eel.init('C:/Users/Zaeem Ahmed/Desktop/algo project/Dynamic-Programming-Algorithms-/Web')
 
 @eel.expose
 def showInput(inputFile):
